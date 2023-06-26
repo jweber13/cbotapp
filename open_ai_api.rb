8f02f7b7afc2f0056a2ac0e23765b28965da900c
joe weber <jweber4213@gmail.com>
2023-06-26 16:12:33 +0900

    adjusted prompting

--- open_ai_api.rb
+++ open_ai_api.rb
@@ -3,7 +3,7 @@ require "ruby/openai"
 
 def fetch_chat(prompt)
   api_key = ENV["OPENAI_ACCESS_TOKEN"]
-  prompt_to_send = "you are a coach and you must motivate me to put down my phone and keep training. respond to this statement, and keep your response short: " + prompt
+  prompt_to_send = "you are a coach and you must motivate me to put down my phone and keep training. answer my question first, keep your response short, and remind me that I must train: " + prompt
   client = OpenAI::Client.new(access_token: ENV["OPENAI_ACCESS_TOKEN"])
 
   response = client.chat(
